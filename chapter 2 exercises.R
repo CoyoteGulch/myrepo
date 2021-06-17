042a41e6d59c1a9195d9a673bf44c9fb4bc78a66
CoyoteGulch <coyotegulch@mac.com>
2021-06-17 12:10:55 -0600

    Another commit

---
+++ chapter 2 exercises.R
@@ -0,0 +1,15 @@
+#John Orr
+mtcars_data <- mtcars
+#
+dim(mtcars_data)
+#
+nrow(mtcars_data)
+#
+ncol(mtcars_data)
+#
+ncol(mtcars_data)
+#
+str(mtcars_data)
+#
+summary(mtcars_data)
+# Another comment line
